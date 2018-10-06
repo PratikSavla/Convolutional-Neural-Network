727037d3f48721ca8ec0fb2f80f25273f1afa304
Pratik Savla <pratiksavla07@gmail.com>
2018-10-06 17:42:02 +0530

    Add files via upload

---
+++ predict_app.py
@@ -0,0 +1,55 @@
+import base64
+import numpy as np
+import io
+from PIL import Image
+import keras
+from keras import backend as k
+from keras.models import Sequential
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array
+from flask import request
+from flask import jsonify
+from flask import Flask
+from keras.layers.convolutional import *
+import itertools
+app = Flask(__name__)
+
+def get_model():
+    # load json and create model
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    return loaded_model
+
+def preprocess_image(image, target_size):
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    image = image.resize(target_size)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+
+    return image
+
+print(" * Loading Keras model...")
+model = get_model()
+print(" * Model Loaded!")
+@app.route('/predict', methods=['POST'])
+def predict():
+    message = request.get_json(force=True)
+    encoded = message['image']
+    decoded = base64.b64decode(encoded)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocess_image(image, target_size=(224,224))
+
+    prediction = model.predict(processed_image).tolist()
+    response = {
+        'prediction': {
+            'dog':prediction[0][0],
+            'cat':prediction[0][1]
+        }
+    }
+    return jsonify(response)
